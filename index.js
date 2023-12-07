d61979c1c7a445bbce1271015265795e2b1641a4
Jhonnaa <jonnyemeni@gmail.com>
2023-12-07 12:54:50 -0300

    Updates

--- index.js
+++ index.js
@@ -7,3 +7,73 @@ for (let i = 0; i <=2 ; i++) {
     })
     
 }
+
+
+const parrafos = document.querySelectorAll('.cont-parrafos')
+const titles =  document.querySelectorAll('.title')
+const servis = document.querySelector('.servis')
+const clientes = document.querySelector('.my-projects')
+const form = document.querySelector('.cont-form')
+
+function aboutAnimation(entries){
+    entries.forEach(entry => {
+        const parrafo = entry.target.querySelector('.p')
+        const title = entry.target.querySelector('.title')
+        parrafo.classList.toggle('unset',entry.isIntersecting)
+        title.classList.toggle('unset',entry.isIntersecting)
+
+    });}
+
+    function carruselAnimation(entries){
+        entries.forEach(entry => {
+            const title = entry.target.querySelector('.title')
+            const carrusel = entry.target.querySelector('.cont-carrusel-servis')
+            title.classList.toggle('unset',entry.isIntersecting)
+            carrusel.classList.toggle('unset',entry.isIntersecting)
+        
+        });
+    }
+
+
+        function clientsAnimation(entries){
+            entries.forEach(entry => {
+                const title = entry.target.querySelector('.title')
+                const client = entry.target.querySelector('.img-clients')
+                title.classList.toggle('unset',entry.isIntersecting)
+                client.classList.toggle('unset',entry.isIntersecting)
+                
+            });
+
+}
+
+function formAnimation(entries){
+    entries.forEach(entry => {
+        const title = entry.target.querySelector('.title')
+        const form = entry.target.querySelector('.form')
+        title.classList.toggle('unset',entry.isIntersecting)
+        form.classList.toggle('unset',entry.isIntersecting)
+        
+    });
+
+}
+
+
+const options ={
+    root: null,
+    rootMargin: "0px",
+    threshold: .25
+}
+
+
+const observerAbout = new IntersectionObserver(aboutAnimation, options);
+const observerCarrusel = new IntersectionObserver(carruselAnimation, options);
+const observerClients = new IntersectionObserver(clientsAnimation, options);
+const observerForm = new IntersectionObserver(formAnimation, options);
+
+parrafos.forEach(parrafo=>{
+    observerAbout.observe(parrafo)
+})
+
+    observerCarrusel.observe(servis)
+    observerClients.observe(clientes)
+    observerForm.observe(form)
